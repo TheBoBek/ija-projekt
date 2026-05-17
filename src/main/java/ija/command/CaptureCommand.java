84b8471761cc9727ae2272e8665f414e9a521c99
alegzoo <alexaliascasillas@gmail.com>
2026-05-17 11:28:26 +0200

    Add capture command

---
+++ src/main/java/ija/command/CaptureCommand.java
@@ -0,0 +1,32 @@
+package ija.command;
+
+import ija.common.Position;
+import ija.game.CaptureResult;
+import ija.game.Game;
+
+/**
+ * Perform capture action and consume the unit action (wait) if game continues.
+ */
+public final class CaptureCommand implements GameCommand<CaptureResult> {
+    private final Position unitPosition;
+
+    public CaptureCommand(Position unitPosition) {
+        this.unitPosition = unitPosition;
+    }
+
+    @Override
+    public CommandResult<CaptureResult> execute(Game game) {
+        CaptureResult result = game.captureBuilding(unitPosition);
+        boolean stateChanged = result.progressApplied();
+        if (result.progressApplied() && !game.isGameOver()) {
+            boolean waited = game.waitUnit(unitPosition);
+            stateChanged = stateChanged || waited;
+        }
+        return new CommandResult<>(result, stateChanged);
+    }
+
+    @Override
+    public String describe() {
+        return "Capture " + unitPosition;
+    }
+}
